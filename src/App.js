82dec3726e3820d45bff287a4680510bbeb900bc
JesusRRr <rolonjesus1996@gmail.com>
2020-05-21 14:38:00 -0500

    feature: store and reucers were created and Provider component was added to App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
-import Results from './components/results/Results'
-import Details from './components/details/Details'
+import Results from './components/results/Results';
+import Details from './components/details/Details';
+import store from './redux/store';
 
 const App = () => {
     return (
         <div>
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/results" component={Results}/>
-                    <Route path="/details/:itemId" component={Details}/>
-                    <Redirect from="/" to="/results"/>
-                </Switch>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/results" component={Results}/>
+                        <Route path="/details/:itemId" component={Details}/>
+                        <Redirect from="/" to="/results"/>
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
         </div>
+
     )
 }
 export default App
