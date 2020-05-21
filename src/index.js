62b75ba345d998b03018a97b35265a5bb302eb32
JesusRRr <rolonjesus1996@gmail.com>
2020-05-21 13:33:14 -0500

    chore: import Provider from Redux and Router from router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
