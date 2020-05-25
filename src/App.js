390a848e56d19dea1cddb486b9fe465d79ec8d15
JesusRRr <rolonjesus1996@gmail.com>
2020-05-25 17:57:42 -0500

    chore: TodoList and TodoInput created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 
 const App = () => {
     return (
         <div>
-            <h1>Hello world</h1>
+            <TodoInput/>
+            <TodoList/> 
         </div>
     )
 }
